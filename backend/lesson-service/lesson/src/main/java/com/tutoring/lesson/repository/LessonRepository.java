b5b89edd4784074d7adf8f619f99ef1a13aeb8ee
Grodelek <arczigrodel@wp.pl>
2025-06-14 16:29:41 +0200

    lesson microservice

---
+++ backend/lesson-service/lesson/src/main/java/com/tutoring/lesson/repository/LessonRepository.java
@@ -0,0 +1,9 @@
+package com.tutoring.lesson.repository;
+
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.tutoring.lesson.model.Lesson;
+
+public interface LessonRepository extends JpaRepository<Lesson, UUID> {
+
+}
