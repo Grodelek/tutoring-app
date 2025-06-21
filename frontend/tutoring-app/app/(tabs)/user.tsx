b462bf87fcc5a1889966ab94efb997eb7f59f4e9
Grodelek <arczigrodel@wp.pl>
2025-06-22 00:05:45 +0200

    changes

---
+++ frontend/tutoring-app/app/(tabs)/user.tsx
@@ -0,0 +1,73 @@
+import React, {useState } from 'react';
+import { View, TextInput, Button, Alert, StyleSheet } from 'react-native';
+
+const UserForm: React.FC = () => {
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async () => {
+    try {
+      const response = await fetch('http://192.168.1.32:8090/api/users/add', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, username, password }),
+      });
+    if (response.ok) {
+      Alert.alert('Success', 'User registered successfully!');
+    } else {
+      const errorText = await response.text();
+      Alert.alert('Error', `User registration failed ${errorText}`);
+    }
+    }catch (error){
+      Alert.alert('Error', `Problem with connection: ${error}`);
+    }
+};
+return (
+  <View style = {styles.container}>
+    <TextInput
+      placeholder="Email"
+      value={email}
+      onChangeText={setEmail}
+      style={[styles.input, styles.inputTextWhite]}
+      autoCapitalize="none"
+      />
+    <TextInput
+      placeholder="Username"
+      value={username}
+      onChangeText={setUsername}
+      style={[styles.input, styles.inputTextWhite]}
+      autoCapitalize="none"
+      />
+     <TextInput
+      placeholder="Password"
+      value={password}
+      onChangeText={setPassword}
+      style={[styles.input]}
+      autoCapitalize="none"
+      secureTextEntry={true}
+      /> 
+      <Button title="Register" onPress={handleSubmit} />
+  </View>
+);
+};
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    marginTop: 40,
+    backgroundColor: '#000', // opcjonalnie ciemne tło formularza
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#999',
+    padding: 10,
+    marginBottom: 15,
+    borderRadius: 5,
+    color: 'white',               // 👈 biały tekst
+    backgroundColor: '#222',      // 👈 ciemne tło inputa
+  },
+});
+
+export default UserForm;
