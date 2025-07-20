2426b23f08b6386aa016bcff5bca9d0d3e789009
Grodelek <arczigrodel@wp.pl>
2025-07-20 23:16:56 +0200

    backend conversation history changes

--- backend/user-service/app/src/main/java/com/tutoring/app/repository/ConversationRepository.java
+++ backend/user-service/app/src/main/java/com/tutoring/app/repository/ConversationRepository.java
@@ -1,10 +1,20 @@
 package com.tutoring.app.repository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import com.tutoring.app.model.Conversation;
 
+@Repository
 public interface ConversationRepository extends JpaRepository<Conversation, UUID> {
   List<Conversation> findByUser1IdOrUser2Id(UUID user1Id, UUID user2Id);
+
+  @Query("SELECT c FROM Conversation c WHERE " +
+      "(c.user1Id = :userA AND c.user2Id = :userB) OR " +
+      "(c.user1Id = :userB AND c.user2Id = :userA)")
+  Optional<Conversation> findByUsers(@Param("userA") UUID userA, @Param("userB") UUID userB);
 }
